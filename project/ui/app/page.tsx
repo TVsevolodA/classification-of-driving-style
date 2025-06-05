e14975e5579b40e07e18c6889972e1d61bdf27fa
TVsevolodA <tri-vsal@yandex.ru>
2025-06-05 16:55:43 +0400

    Добавил построение маршрута и его визуализацию, с помощью библиотеки maplibre-gl.

---
+++ project/ui/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const RouteMap = dynamic(() => import('../components/RouteMap'), {
+  ssr: false,
+});
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Маршрут с прогрессом в Next.js</h1>
+      <RouteMap />
+    </div>
+  );
+}
