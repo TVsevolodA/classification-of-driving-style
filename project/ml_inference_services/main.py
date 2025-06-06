2b27be4e03bfa887a0cfcace3dc40f4d6445d47c
TVsevolodA <tri-vsal@yandex.ru>
2025-06-06 16:14:48 +0400

    Обновил файл с моделью. Добавил загрузку через pickle.

--- project/ml_inference_services/main.py
+++ project/ml_inference_services/main.py
@@ -1,9 +1,13 @@
+import json
+import pickle
+
 import pandas as pd
-from fastai.tabular.all import *
+# from fastai.tabular.all import load_learner
 from fastapi import Request, FastAPI
 
-MODEL = load_learner('model.pkl')
-DICT_RESULT = {1: 'Agressive', 2: 'Normal', 3: 'Indefinite'}
+# MODEL = load_learner('model1.pickle')
+MODEL = pickle.load(open('model1.pickle', 'rb'))
+DICT_RESULT = {1: 'Agressive', 2: 'Normal', 3: 'Vague'}
 
 app = FastAPI()
 
@@ -23,67 +27,91 @@ async def predict(request: Request):
 async def predict():
     return json.dumps({"response": "все работает!"})
 
-# learn = load_learner('model.pkl')
+# learn = pickle.load(open('model1.pickle', 'rb'))
+# # learn = load_learner('model.pkl')
 #
 # examples_driving_styles = \
 #  {
-#      "агрессивный": {
-#          "veincle length": 450,
-#          "veincle weight": 2500,
-#          "axles number": 2,
-#          "perceding veincle time-gap": 0.5,
-#          "Lane of the road": 2,
-#          "veincle speed": 200,
-#          "perceding veincle speed": 150,
-#          "perceding veincle weight": 1400,
-#          "perceding veincle length": 440,
-#          "road condition": 2,
-#          "Air temprture": -5,
-#          "perciption type": 2,
-#          "perciption intensity": 2,
-#          "relatve humadity": 40,
-#          "wind direction": 90,
-#          "wind speed": 25,
-#          "Lighting condition": 2
-#          },
-#      "нормальный": {
-#          "veincle length": 460,
-#          "veincle weight": 1600,
-#          "axles number": 2,
-#          "perceding veincle time-gap": 2.5,
-#          "Lane of the road": 2,
-#          "veincle speed": 90,
-#          "perceding veincle speed": 85,
-#          "perceding veincle weight": 1500,
-#          "perceding veincle length": 450,
-#          "road condition": 1,
-#          "Air temprture": 15,
-#          "perciption type": 1,
-#          "perciption intensity": 1,
-#          "relatve humadity": 70,
-#          "wind direction": 180,
-#          "wind speed": 3,
-#          "Lighting condition": 1
-#          },
-#      "неопределенный": {
-#          "veincle length": 470,
-#          "veincle weight": 1700,
-#          "axles number": 2,
-#          "perceding veincle time-gap": 3.5,
-#          "Lane of the road": 1,
-#          "veincle speed": 70,
-#          "perceding veincle speed": 65,
-#          "perceding veincle weight": 1600,
-#          "perceding veincle length": 460,
-#          "road condition": 2,
-#          "Air temprture": 0,
-#          "perciption type": 2,
-#          "perciption intensity": 2,
-#          "relatve humadity": 85,
-#          "wind direction": 270,
-#          "wind speed": 4,
-#          "Lighting condition": 2
-#          }
+#      # Пример 1:
+#      # "нормальный": {
+#      #     "veincle length": 350, # Длина тс (см)
+#      #     "veincle weight": 2500, # Вес тс (кг)
+#      #     "axles number": 2, # Количество осей (2-9)
+#      #     "perceding veincle time-gap": 2, # время проезда предыдущего тс (1-1800 сек)
+#      #     "Lane of the road": 2, # Полоса движения на дороге (1 или 2)
+#      #     "veincle speed": 60, # Скорость (8-160)
+#      #     "perceding veincle speed": 70, # Скорость предыдущего тс (0-160)
+#      #     "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#      #     "perceding veincle length": 250, # Длина предыдущего тс (100-3000 см)
+#      #     "road condition": 2, # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#      #     "Air temprture": -5, # Температура воздуха (-13 - +24)
+#      #     "perciption type": 0, # Облачность: 0-ясно, 1-дождь, 2-снег
+#      #     "perciption intensity": 0, # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#      #     "relatve humadity": 30, # Относительная влажность воздуха (16-97)
+#      #     "wind direction": 20, # Направление ветра 0-360 (6-360)
+#      #     "wind speed": 5, # Скорость ветра (0-17)
+#      #     "Lighting condition": 2 # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      #     },
+#      # "агрессивный": {
+#      #     "veincle length": 350, # Длина тс (см)
+#      #     "veincle weight": 2500, # Вес тс (кг)
+#      #     "axles number": 2, # Количество осей (2-9)
+#      #     "perceding veincle time-gap": 2, # время проезда предыдущего тс (1-1800 сек)
+#      #     "Lane of the road": 2, # Полоса движения на дороге (1 или 2)
+#      #     "veincle speed": 160, # Скорость (8-160)
+#      #     "perceding veincle speed": 70, # Скорость предыдущего тс (0-160)
+#      #     "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#      #     "perceding veincle length": 250, # Длина предыдущего тс (100-3000 см)
+#      #     "road condition": 2, # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#      #     "Air temprture": -5, # Температура воздуха (-13 - +24)
+#      #     "perciption type": 0, # Облачность: 0-ясно, 1-дождь, 2-снег
+#      #     "perciption intensity": 0, # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#      #     "relatve humadity": 30, # Относительная влажность воздуха (16-97)
+#      #     "wind direction": 20, # Направление ветра 0-360 (6-360)
+#      #     "wind speed": 5, # Скорость ветра (0-17)
+#      #     "Lighting condition": 2 # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      #     },
+#      # Пример 2:
+#      "агр": {
+#          "veincle length": 950,  # Длина тс (см)
+#          "veincle weight": 5000,  # Вес тс (кг)
+#          "axles number": 4,  # Количество осей (2-9)
+#          "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
+#          "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
+#          "veincle speed": 140,  # Скорость (8-160)
+#          "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
+#          "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#          "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
+#          "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#          "Air temprture": 15,  # Температура воздуха (-13 - +24)
+#          "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
+#          "perciption intensity": 3,
+#          # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#          "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
+#          "wind direction": 50,  # Направление ветра 0-360 (6-360)
+#          "wind speed": 15,  # Скорость ветра (0-17)
+#          "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      },
+#      "норм": {
+#          "veincle length": 950,  # Длина тс (см)
+#          "veincle weight": 5000,  # Вес тс (кг)
+#          "axles number": 4,  # Количество осей (2-9)
+#          "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
+#          "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
+#          "veincle speed": 90,  # Скорость (8-160)
+#          "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
+#          "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#          "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
+#          "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#          "Air temprture": 15,  # Температура воздуха (-13 - +24)
+#          "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
+#          "perciption intensity": 3,
+#          # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#          "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
+#          "wind direction": 50,  # Направление ветра 0-360 (6-360)
+#          "wind speed": 15,  # Скорость ветра (0-17)
+#          "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      },
 #      }
 #
 # dict_result = {1: 'Агрессивный', 2: 'Нормальный', 3: 'Неопределенный'}
@@ -95,12 +123,14 @@ async def predict():
 #     "perceding veincle length", "road condition", "Air temprture",
 #     "perciption type", "perciption intensity", "relatve humadity",
 #     "wind direction", "wind speed", "Lighting condition"]
-
+#
 # for value in examples_driving_styles.values():
-#   input_data = pd.Series(
-#       data=value.values(),
-#       index=columns
-#   )
-#   row, clas, probs = learn.predict(input_data)
-#   number_class = int( row["DrivingStyle"][0] )
-#   print( dict_result.get(number_class) )
+#     result = learn.predict([list(value.values())])
+#     print(dict_result.get(result[0]))
+#   # input_data = pd.Series(
+#   #     data=value.values(),
+#   #     index=columns
+#   # )
+#   # row, clas, probs = learn.predict(input_data)
+#   # number_class = int( row["DrivingStyle"][0] )
+#   # print( dict_result.get(number_class) )
