832b6da269721e20bd16bd212c6b7102652e77f2
TVsevolodA <tri-vsal@yandex.ru>
2025-06-17 19:26:26 +0400

    Наладил потоковое вещание с параллельным предсказанием из нейросети.

--- project/device_emulator/main.py
+++ project/device_emulator/main.py
@@ -1,6 +1,74 @@
-from websocket import create_connection, WebSocket
+import datetime as dt
+from time import sleep
 
+from websocket import create_connection, WebSocket
+import subprocess
+
+# 1. Запустить трансляцию
+cmd = [
+    'ffmpeg', '-re', '-stream_loop', '-1',
+    '-i', 'normal_test_drive.MP4',
+    '-c:v', 'libx264', '-tune', 'zerolatency',
+    '-preset', 'ultrafast', '-x264-params',
+    'bframes=0', '-g', '30', '-keyint_min', '30',
+    '-sc_threshold', '0', '-c:a', 'aac',
+    '-s', '1280x720',
+    '-f', 'flv', 'rtmp://device_emulator:5000/hls/xxx'
+]
+# process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+subprocess.Popen(cmd)
+
+# 2. Установить соединение с gateway
 ws: WebSocket = create_connection("ws://gateway:7000/tracking")
-ws.send('{"veincle length": 450, "veincle weight": 2500, "axles number": 2, "perceding veincle time-gap": 0.5, "Lane of the road": 2, "veincle speed": 200, "perceding veincle speed": 150, "perceding veincle weight": 1400, "perceding veincle length": 440, "road condition": 2, "Air temprture": -5, "perciption type": 2, "perciption intensity": 2, "relatve humadity": 40, "wind direction": 90, "wind speed": 25, "Lighting condition": 2}')
-result =  ws.recv()
-print(f"Сервер вернул\n{result}")
+
+
+# 3. Засечь время запуска
+start_time = dt.datetime.now()
+
+with open('normal_test_drive.txt', 'r') as data_file:
+    MIN_TIME = int(data_file.readline().split(',')[0])
+
+def get_speed(timecode: int) -> int:
+    """
+    Получение скорости транспортного средства в конкретный момент времени
+    :param timecode: Время за записи
+    :return: current_speed: Скорость в данный момент времени
+    """
+    with open('normal_test_drive.txt', 'r') as gps_data:
+        CURRENT_TIME = MIN_TIME + timecode
+        for data in gps_data:
+            if int( data.split(',')[0] ) >= CURRENT_TIME:
+                speed_cm_s = int( data.split(',')[5] )
+                speed_km_h = round( speed_cm_s * 3600 / 1e5 )  # 1e5 == 100_000
+                return speed_km_h
+        return 0
+
+while True:
+    current_time = dt.datetime.now()
+    diff_time = (current_time - start_time).total_seconds()
+    if diff_time >= 60:
+        start_time = current_time
+    current_speed = get_speed( timecode=int(diff_time % 60) )
+
+    messege = [
+        '{"veincle length": 441,',
+        ' "veincle weight": 1380,',
+        ' "axles number": 2,',
+        ' "perceding veincle time-gap": 60,',
+        ' "Lane of the road": 1,',
+        f' "veincle speed": {current_speed},',
+        ' "perceding veincle speed": 30,',
+        ' "perceding veincle weight": 2021,',
+        ' "perceding veincle length": 500,',
+        ' "road condition": 0,',
+        ' "Air temprture": 25,',
+        ' "perciption type": 0,',
+        ' "perciption intensity": -1,',
+        ' "relatve humadity": 60,',
+        ' "wind direction": 50,',
+        ' "wind speed": 8,',
+        ' "Lighting condition": 0}',
+    ]
+    ws.send(''.join(messege))
+    result = ws.recv()
+    print(f"Сервер вернул, что на скорости {current_speed} \n{result}")
--- project/ml_inference_services/main.py
+++ project/ml_inference_services/main.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 # from fastai.tabular.all import load_learner
 from fastapi import Request, FastAPI
+from fastapi.responses import JSONResponse
 
 # MODEL = load_learner('model1.pickle')
 MODEL = pickle.load(open('model1.pickle', 'rb'))
@@ -14,14 +15,11 @@ app = FastAPI()
 @app.post("/predict")
 async def predict(request: Request):
     data = await request.json()
-    input_data = pd.Series(
-        data=data.values(),
-        index=data.keys()
-    )
-    row, clas, probs = MODEL.predict(input_data)
-    number_class: int = int( row["DrivingStyle"][0] )
-    prediction_result: str = DICT_RESULT.get(number_class)
-    return json.dumps({ "prediction_result": prediction_result })
+    if len(data) == 0:
+        return JSONResponse(content={}, status_code=400)
+    result = MODEL.predict([list(data.values())])
+    prediction_result = DICT_RESULT.get(result[0])
+    return JSONResponse(content={"prediction_result": prediction_result}, status_code=200)
 
 @app.get("/")
 async def predict():
@@ -72,44 +70,64 @@ async def predict():
 #      #     "Lighting condition": 2 # Условия освещения: 0-день, 1-ночь, 2-сумерки
 #      #     },
 #      # Пример 2:
-#      "агр": {
-#          "veincle length": 950,  # Длина тс (см)
-#          "veincle weight": 5000,  # Вес тс (кг)
-#          "axles number": 4,  # Количество осей (2-9)
-#          "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
-#          "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
-#          "veincle speed": 140,  # Скорость (8-160)
-#          "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
-#          "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
-#          "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
-#          "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
-#          "Air temprture": 15,  # Температура воздуха (-13 - +24)
-#          "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
-#          "perciption intensity": 3,
-#          # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
-#          "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
-#          "wind direction": 50,  # Направление ветра 0-360 (6-360)
-#          "wind speed": 15,  # Скорость ветра (0-17)
-#          "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
-#      },
+#      # "агр": {
+#      #     "veincle length": 950,  # Длина тс (см)
+#      #     "veincle weight": 5000,  # Вес тс (кг)
+#      #     "axles number": 4,  # Количество осей (2-9)
+#      #     "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
+#      #     "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
+#      #     "veincle speed": 140,  # Скорость (8-160)
+#      #     "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
+#      #     "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#      #     "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
+#      #     "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#      #     "Air temprture": 15,  # Температура воздуха (-13 - +24)
+#      #     "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
+#      #     "perciption intensity": 3,
+#      #     # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#      #     "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
+#      #     "wind direction": 50,  # Направление ветра 0-360 (6-360)
+#      #     "wind speed": 15,  # Скорость ветра (0-17)
+#      #     "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      # },
+#      # "норм": {
+#      #     "veincle length": 950,  # Длина тс (см)
+#      #     "veincle weight": 5000,  # Вес тс (кг)
+#      #     "axles number": 4,  # Количество осей (2-9)
+#      #     "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
+#      #     "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
+#      #     "veincle speed": 90,  # Скорость (8-160)
+#      #     "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
+#      #     "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
+#      #     "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
+#      #     "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#      #     "Air temprture": 15,  # Температура воздуха (-13 - +24)
+#      #     "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
+#      #     "perciption intensity": 3,
+#      #     # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
+#      #     "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
+#      #     "wind direction": 50,  # Направление ветра 0-360 (6-360)
+#      #     "wind speed": 15,  # Скорость ветра (0-17)
+#      #     "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
+#      # },
 #      "норм": {
-#          "veincle length": 950,  # Длина тс (см)
-#          "veincle weight": 5000,  # Вес тс (кг)
-#          "axles number": 4,  # Количество осей (2-9)
-#          "perceding veincle time-gap": 600,  # время проезда предыдущего тс (1-1800 сек)
+#          "veincle length": 441,  # Длина тс (см)
+#          "veincle weight": 1380,  # Вес тс (кг)
+#          "axles number": 2,  # Количество осей (2-9)
+#          "perceding veincle time-gap": 60,  # время проезда предыдущего тс (1-1800 сек)
 #          "Lane of the road": 1,  # Полоса движения на дороге (1 или 2)
-#          "veincle speed": 90,  # Скорость (8-160)
-#          "perceding veincle speed": 80,  # Скорость предыдущего тс (0-160)
-#          "perceding veincle weight": 1500,  # Вес предыдущего тс (3-70000 кг)
-#          "perceding veincle length": 250,  # Длина предыдущего тс (100-3000 см)
-#          "road condition": 1,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
-#          "Air temprture": 15,  # Температура воздуха (-13 - +24)
-#          "perciption type": 1,  # Облачность: 0-ясно, 1-дождь, 2-снег
-#          "perciption intensity": 3,
+#          "veincle speed": 35,  # Скорость (8-160)
+#          "perceding veincle speed": 30,  # Скорость предыдущего тс (0-160)
+#          "perceding veincle weight": 2021,  # Вес предыдущего тс (3-70000 кг)
+#          "perceding veincle length": 500,  # Длина предыдущего тс (100-3000 см)
+#          "road condition": 0,  # Состояние дороги: 0-сухая, 1-мокрая, 2-видимые следы, 3-заснеженно
+#          "Air temprture": 25,  # Температура воздуха (-13 - +24)
+#          "perciption type": 0,  # Облачность: 0-ясно, 1-дождь, 2-снег
+#          "perciption intensity": -1,
 #          # Скорость выпадения осадков мм/10мин: -1-нет, 0-низкие (0-1), 1-умеренные (1-5), 2-большие (5-16) 3
-#          "relatve humadity": 90,  # Относительная влажность воздуха (16-97)
+#          "relatve humadity": 60,  # Относительная влажность воздуха (16-97)
 #          "wind direction": 50,  # Направление ветра 0-360 (6-360)
-#          "wind speed": 15,  # Скорость ветра (0-17)
+#          "wind speed": 8,  # Скорость ветра (0-17)
 #          "Lighting condition": 0  # Условия освещения: 0-день, 1-ночь, 2-сумерки
 #      },
 #      }
@@ -127,10 +145,10 @@ async def predict():
 # for value in examples_driving_styles.values():
 #     result = learn.predict([list(value.values())])
 #     print(dict_result.get(result[0]))
-#   # input_data = pd.Series(
-#   #     data=value.values(),
-#   #     index=columns
-#   # )
-#   # row, clas, probs = learn.predict(input_data)
-#   # number_class = int( row["DrivingStyle"][0] )
-#   # print( dict_result.get(number_class) )
+#     # input_data = pd.Series(
+#     # data=value.values(),
+#     # index=columns
+#     # )
+#     # row, clas, probs = learn.predict(input_data)
+#     # number_class = int( row["DrivingStyle"][0] )
+#     # print( dict_result.get(number_class) )
